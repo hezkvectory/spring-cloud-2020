f45e9cbb8dfd170deeec40d2d5d225fcc1c2ca12
hezhengkui <hezhengkui@vipkid.com.cn>
2021-01-05 19:29:34 +0800

    day3

---
+++ commons/src/main/java/com/hezk/commons/configuration/CommonsEnvironmentPostProcessor.java
@@ -0,0 +1,41 @@
+package com.hezk.commons.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonsEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    public static final String SOURCE_NAME_FIRST = "commons config";
+
+    private final Map<String, Object> firstSource = new HashMap<>();
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        MutablePropertySources propertySources = environment.getPropertySources();
+        //已装载，则中断
+        if (propertySources.contains(SOURCE_NAME_FIRST)) {
+            return;
+        }
+
+        MapPropertySource firstPropertySource = new MapPropertySource(SOURCE_NAME_FIRST, firstSource);
+        propertySources.addFirst(firstPropertySource);
+
+        firstSource.put("management.endpoint.prometheus.enabled", true);
+        firstSource.put("management.endpoints.web.exposure.include", "*");
+
+        firstSource.put("spring.zipkin.enabled", false);
+        firstSource.put("spring.zipkin.base-url", "http://localhost:9411/");
+        firstSource.put("spring.sleuth.sampler.probability", 1.0);
+
+        firstSource.put("eureka.client.register-with-eureka", true);
+        firstSource.put("eureka.client.fetch-registry", true);
+        firstSource.put("eureka.client.service-url.default-zone", "http://localhost:7001/eureka");
+    }
+
+}
--- commons/src/main/java/com/hezk/commons/configuration/WebAutoConfiguration.java
+++ commons/src/main/java/com/hezk/commons/configuration/WebAutoConfiguration.java
@@ -1,24 +1,39 @@
 package com.hezk.commons.configuration;
 
-import com.hezk.commons.filter.ProfilerHttpFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 public class WebAutoConfiguration {
 
     @Bean
-    @ConditionalOnProperty(value = "login.default.filter.enabled", havingValue = "true")
-    public FilterRegistrationBean meteorLoginFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        ProfilerHttpFilter filter = new ProfilerHttpFilter();
-        registration.setFilter(filter);
-        registration.addUrlPatterns("/*");
-        registration.setName("profilerHttpFilter");
-        registration.setOrder(filter.getOrder());
-        return registration;
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().meterFilter(new MeterFilter() {
+            @Override
+            public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+                if (id.getType() == Meter.Type.TIMER) {
+                    return DistributionStatisticConfig.builder()
+                            .percentilesHistogram(true)
+                            .percentiles(0.5, 0.90, 0.95, 0.99)
+                            .sla(Duration.ofMillis(100).toNanos(),
+                                    Duration.ofMillis(200).toNanos(),
+                                    Duration.ofSeconds(1).toNanos(),
+                                    Duration.ofSeconds(5).toNanos())
+                            .minimumExpectedValue(Duration.ofMillis(20).toNanos())
+                            .maximumExpectedValue(Duration.ofSeconds(5).toNanos())
+                            .build()
+                            .merge(config);
+                } else {
+                    return config;
+                }
+            }
+        });
     }
-
 }
