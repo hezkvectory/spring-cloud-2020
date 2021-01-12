38464e203c9f169db01f8f910a6ad1d96319b5c6
hezhengkui <hezhengkui@vipkid.com.cn>
2021-01-12 18:03:32 +0800

    时间转换

--- shiro/src/main/java/com/hezk/shiro/configuration/properties/UserProperties.java
+++ shiro/src/main/java/com/hezk/shiro/configuration/properties/UserProperties.java
@@ -1,13 +1,19 @@
 package com.hezk.shiro.configuration.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @ConfigurationProperties(prefix = "user")
 public class UserProperties {
     private String username;
     private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-ss HH:mm:dd", iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime currentTime;
 
     public String getUsername() {
         return username;
@@ -23,5 +29,13 @@ public class UserProperties {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public LocalDateTime getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(LocalDateTime currentTime) {
+        this.currentTime = currentTime;
     }
 }
